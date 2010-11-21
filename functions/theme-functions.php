822de4b22daffb3c3329064cc73e3e070f1ddc53
Devin Price <devin@wptheming.com>
2010-11-21 16:50:29 -0600

    Initial Commit

---
+++ functions/theme-functions.php
@@ -0,0 +1,155 @@
+<?php
+
+/* These are functions specific to these options settings and this theme */
+
+/*-----------------------------------------------------------------------------------*/
+/* Remove the Options Panel that Thematic comes with by Default
+/*-----------------------------------------------------------------------------------*/
+
+function remove_thematic_panel() {
+  remove_action('admin_menu' , 'mytheme_add_admin');
+}
+add_action('init', 'remove_thematic_panel');
+
+/*-----------------------------------------------------------------------------------*/
+/* Theme Header Output - wp_head() */
+/*-----------------------------------------------------------------------------------*/
+
+// This sets up the layouts and styles selected from the options panel
+
+if (!function_exists(optionsframework_wp_head)) {
+	function optionsframework_wp_head() { 
+	
+		//Layouts
+		 $layout = get_option('of_layout');
+		 if ($layout == '') {
+		 	$layout = '2c-r-fixed';
+		 }
+	     echo '<link href="'. get_bloginfo('stylesheet_directory') .'/layouts/'. $layout . '.css" rel="stylesheet" type="text/css" />'."\n";
+	    
+		//Styles
+		 if(!isset($_REQUEST['style']))
+		 	$style = ''; 
+		 else 
+	     	$style = $_REQUEST['style'];
+	     if ($style != '') {
+			  $GLOBALS['stylesheet'] = $style;
+	          echo '<link href="'. get_bloginfo('stylesheet_directory') .'/styles/'. $GLOBALS['stylesheet'] . '.css" rel="stylesheet" type="text/css" />'."\n"; 
+	     } else { 
+	          $GLOBALS['stylesheet'] = get_option('of_alt_stylesheet');
+	          if($GLOBALS['stylesheet'] != '')
+	               echo '<link href="'. get_bloginfo('stylesheet_directory') .'/styles/'. $GLOBALS['stylesheet'] .'" rel="stylesheet" type="text/css" />'."\n";         
+	          else
+	               echo '<link href="'. get_bloginfo('stylesheet_directory') .'/styles/default.css" rel="stylesheet" type="text/css" />'."\n";         		  
+	     }       
+			
+		// This prints out the custom css and specific styling options
+		of_head_css();
+	}
+}
+
+add_action('wp_head', 'optionsframework_wp_head');
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Output CSS from standarized options */
+/*-----------------------------------------------------------------------------------*/
+
+function of_head_css() {
+
+		$shortname =  get_option('of_shortname'); 
+		$output = '';
+		
+		if ($body_color = get_option($shortname . '_body_background') ) {
+			$output .= "body {background:" . $body_color .";}\n";
+		}
+		
+		if ($link_color = get_option($shortname .'_header_background') ) {
+			$output .= "#header {background:" . $link_color .";}\n";
+		}
+		
+		if ($link_hover_color = get_option($shortname .'_footer_background') ) {
+			$output .= "#footer {background:" . $link_hover_color .";}\n";
+		}
+		
+		$custom_css = get_option('of_custom_css');
+		
+		if ($custom_css <> '') {
+			$output .= $custom_css . "\n";
+		}
+		
+		// Output styles
+		if ($output <> '') {
+			$output = "<!-- Custom Styling -->\n<style type=\"text/css\">\n" . $output . "</style>\n";
+			echo $output;
+		}
+	
+}
+
+/*-----------------------------------------------------------------------------------*/
+/* Add Favicon
+/*-----------------------------------------------------------------------------------*/
+
+function childtheme_favicon() {
+		if (get_option('of_custom_favicon') != '') {
+	        echo '<link rel="shortcut icon" href="'.  get_option('of_custom_favicon')  .'"/>'."\n";
+	    }
+		else { ?>
+			<link rel="shortcut icon" href="<?php echo bloginfo('stylesheet_directory') ?>/functions/images/favicon.ico" />
+<?php }
+}
+
+add_action('wp_head', 'childtheme_favicon');
+
+/*-----------------------------------------------------------------------------------*/
+/* Replace Blog Title With Logo
+/*-----------------------------------------------------------------------------------*/
+
+// If a logo is uploaded, unhook the page title and description
+
+function add_childtheme_logo() {
+	$logo = get_option('of_logo');
+	if (!empty($logo)) {
+		remove_action('thematic_header','thematic_blogtitle', 3);
+		remove_action('thematic_header','thematic_blogdescription',5);
+		add_action('thematic_header','childtheme_logo', 3);
+	}
+}
+add_action('init','add_childtheme_logo');
+
+// Displays the logo
+
+function childtheme_logo() {
+	$logo = get_option('of_logo');
+    $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div';?>
+    <<?php echo $heading_tag; ?> id="site-title">
+	<a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('description'); ?>">
+    <img src="<?php echo $logo; ?>" alt="<?php bloginfo('name'); ?>"/>
+	</a>
+    </<?php echo $heading_tag; ?>>
+<?php }
+
+ 
+/*-----------------------------------------------------------------------------------*/
+/* Filter Footer Text
+/*-----------------------------------------------------------------------------------*/
+
+function childtheme_footer($thm_footertext) {
+	if ($footertext = get_option('of_footer_text'))
+    	return $footertext;
+}
+
+add_filter('thematic_footertext', 'childtheme_footer');
+
+/*-----------------------------------------------------------------------------------*/
+/* Show analytics code in footer */
+/*-----------------------------------------------------------------------------------*/
+
+function childtheme_analytics(){
+	$output = get_option('of_google_analytics');
+	if ( $output <> "" ) 
+		echo stripslashes($output) . "\n";
+}
+add_action('wp_footer','childtheme_analytics');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1038\cocoasubrtf350
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww16640\viewh10080\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\ql\qnatural\pardirnatural
+
+\f0\fs24 \cf0 The Thematic Options Panel is a framework to help developers build options into their themes.  To view a video demo of how it works and read the documentation, please visit: http://wptheming.com/thematic-options-panel.\
+\
+Version 2.0 of this theme was completely recoded using WooThemes Canvas as a base (http://www.woothemes.com/2010/02/canvas/).  The Thematic Options Panel is actually a slimmed down version of their version in Canvas, and I'd encourage you to check out and support WooThemes if you find this project helpful.}
